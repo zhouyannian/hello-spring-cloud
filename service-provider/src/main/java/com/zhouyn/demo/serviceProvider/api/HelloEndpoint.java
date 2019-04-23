1d268eb8c5e98f285c520219af86b206bdf032bc
周延年 <zhouyn@asiainfo.com>
2019-04-24 00:44:02 +0800

    init commit

---
+++ service-provider/src/main/java/com/zhouyn/demo/serviceProvider/api/HelloEndpoint.java
@@ -0,0 +1,27 @@
+package com.zhouyn.demo.serviceProvider.api;
+
+import com.netflix.appinfo.EurekaInstanceConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloEndpoint {
+    protected Logger logger = LoggerFactory.getLogger(HelloEndpoint.class);
+
+    @Autowired
+    private EurekaInstanceConfig eurekaInstanceConfig;
+    @Value("${server.port}")
+    private int serverPort = 0;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
+        this.logger.info("/hello, instanceId:{}, host:{}", eurekaInstanceConfig.getInstanceId(), eurekaInstanceConfig.getHostName(false));
+        return "Hello, Spring Cloud! My port is " + String.valueOf(serverPort);
+    }
+}
