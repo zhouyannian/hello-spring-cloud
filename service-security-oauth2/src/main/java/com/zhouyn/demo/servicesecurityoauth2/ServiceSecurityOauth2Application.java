19dd068ea8ae2583508ce8c6d95477ff4cd6a660
周延年 <zhouyn@asiainfo.com>
2019-05-05 23:10:59 +0800

    add eureka,ribbon,feign,hystrix and turbine.

---
+++ service-security-oauth2/src/main/java/com/zhouyn/demo/servicesecurityoauth2/ServiceSecurityOauth2Application.java
@@ -0,0 +1,13 @@
+package com.zhouyn.demo.servicesecurityoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceSecurityOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceSecurityOauth2Application.class, args);
+    }
+
+}
---
+++ service-security-oauth2/src/test/java/com/zhouyn/demo/servicesecurityoauth2/ServiceSecurityOauth2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhouyn.demo.servicesecurityoauth2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceSecurityOauth2ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
