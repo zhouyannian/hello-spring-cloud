2e6b51bdaee413143d6d09551dbee3d5bcd6987a
周延年 <zhouyn@asiainfo.com>
2019-07-14 23:57:13 +0800

    增加单点登录demo。

--- service-consumer/src/main/java/com/zhouyn/demo/serviceConsumer/config/SecurityConfig.java
+++ service-consumer/src/main/java/com/zhouyn/demo/serviceConsumer/config/SecurityConfig.java
@@ -1,55 +1,55 @@
-//package com.zhouyn.demo.serviceConsumer.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//
-//@Configuration
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Bean
-//    @Override
-//    public UserDetailsService userDetailsServiceBean() throws Exception {
-//        return super.userDetailsServiceBean();
-//    }
-//
-//    @Bean
-//    @Override
-//    public AuthenticationManager authenticationManagerBean() throws Exception {
-//        return super.authenticationManagerBean();
-//    }
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        //super.configure(auth);
-//
-//        auth.inMemoryAuthentication()
-//                .withUser("zhouyn")
-//                .password("{noop}zhouyn")
-//                .roles("USER")
-//                .and()
-//                .withUser("admin")
-//                .password("{noop}admin")
-//                .roles("USER","ADMIN");
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        //super.configure(http);
-//        http.csrf().disable();
-//        http.authorizeRequests()
-//                .antMatchers(HttpMethod.GET, "/order/**")
-//                .hasRole("ADMIN")
-//                .anyRequest()
-//                .authenticated()
-//                .and()
-//                .httpBasic();
-//
-//
-//    }
-//}
+package com.zhouyn.demo.serviceConsumer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return super.userDetailsServiceBean();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //super.configure(auth);
+
+        auth.inMemoryAuthentication()
+                .withUser("zhouyn")
+                .password("{noop}zhouyn")
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("{noop}admin")
+                .roles("USER","ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //super.configure(http);
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/order/**")
+                .hasRole("ADMIN")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+
+
+    }
+}
